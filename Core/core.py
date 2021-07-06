e8a0f0a0b25b29049dd1d19d34de9a120f3fe57f
Louis Young <1462648167>
2021-07-07 07:38:31 +0800

    <fix>(core.url): fix url uncorrect

--- Core/core.py
+++ Core/core.py
@@ -10,7 +10,7 @@ from Settings.settings import DEBUG, SERVER_HOST, PORT
 def main():
     bigLearn = BigLearn()
     classList = ClassExistsList()
-    changeInfoURL = "http://{}:{}/changeInfo".format(SERVER_HOST, PORT)
+    changeInfoURL = "http://{}:{}/change.html".format(SERVER_HOST, PORT)
     for classID in classList:
         remindMessage = bigLearn.classDoNotList(classID) + "\n申请更改大学习信息:\n" + changeInfoURL
         groupID = ClassGroupID(classID)
