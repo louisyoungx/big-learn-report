e9755d557ff777a98470e743671ae58c272654fa
Louis Young <1462648167>
2021-07-07 07:31:48 +0800

    <feat>(api.changeInfo): send change info message to admin

--- Core/core.py
+++ Core/core.py
@@ -4,14 +4,15 @@ import time
 from BigLearn.biglearn import BigLearn
 from ClassData.DataAPI import ClassExistsList, ClassGroupID
 from Message.message import sendGroupMessage, sendFriendMessage
-from Settings.settings import DEBUG
+from Settings.settings import DEBUG, SERVER_HOST, PORT
 
 
 def main():
     bigLearn = BigLearn()
     classList = ClassExistsList()
+    changeInfoURL = "http://{}:{}/changeInfo".format(SERVER_HOST, PORT)
     for classID in classList:
-        remindMessage = bigLearn.classDoNotList(classID)
+        remindMessage = bigLearn.classDoNotList(classID) + "申请更改大学习信息:\n" + changeInfoURL
         groupID = ClassGroupID(classID)
         if groupID != "":
             if DEBUG:
